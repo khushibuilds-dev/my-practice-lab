dcb7556665def08936955fa4a1e423d0c760491c
Khushi Singh <khu4656@gmail.com>
2026-08-02 12:18:12 +0530

    practice of the conditional operators

--- my-photo-lab/script.js
+++ my-photo-lab/script.js
@@ -3,3 +3,40 @@ console.log(234);
 console.log("My name is Khushi Singh");
 console.log("I am a web developer");
 console.log("I love to code");
+
+let myAge = 20;
+console.log(myAge);
+// 20 output
+
+let myName;
+console.log(myName);
+//undefined output
+
+let a = 10;
+a = a+1;
+a = a+1;
+console.log(a);
+//output 12
+
+let b =10;
+b += 1;
+b += 1;
+console.log(b);
+//output 12
+
+console.log("Practice of the conditional oprators");
+console.log("there are 3 types of conditional operators");
+console.log("1. if else");
+console.log("2. switch case");
+console.log("3. ternary operator");
+console.log("Practice of the if else statement");
+console.log("if else statement is used to check the condition and execute the code accordingly");
+let age = 20;
+if (age>=18){
+    console.log("You are eligible to vote");
+} 
+else{
+    console.log("You are not elegible to vote");
+}
+
+//output You are eligible to vote

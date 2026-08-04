fe56b7b98908c72fa045baaeba0425ded88991a6
Khushi Singh <khu4656@gmail.com>
2026-08-04 16:33:42 +0530

    structuring the file

---
+++ my-javaScript-folders/javascript-practice-file/script.js
@@ -0,0 +1,31 @@
+console.log("Khushi", "Welcome to my photo lab!", );
+console.log("My Birthday:", 2007);
+console.log("user", "Khushi");
+console.log("My Age:", 16);
+console.log("khushi", + 200);
+let a = "Khushi Singh";
+console.log("My name is;" + " " + a);
+let myAge;
+console.log("My Age is:", myAge);
+let b = true;
+console.log("I am a student:", b);
+let firstName = "Khushi";
+let lastName = "Singh";
+console.log("My full name is;" + " " + firstName + " " + lastName);
+let x = 5;
+x ++;
+console.log(x);
+let y = 10;
+y --;
+console.log(y);
+let z = 17;
+let w = 2;
+let quo = Math.floor(z/w);
+console.log(quo);
+let rem = z%w;
+console.log(rem);
+console.log(z*w);
+console.log(z+w);
+
+
+

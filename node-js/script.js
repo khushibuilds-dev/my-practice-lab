5c41de3ae8977e7f58c4a99ad3a018a64edf24a4
Khushi Singh <khu4656@gmail.com>
2026-08-18 17:47:38 +0530

    practiced array

--- node-js/script.js
+++ node-js/script.js
@@ -1,2 +1,18 @@
 console.log("Helo khushi future tech");
 console.log("Khushi songh");
+let fruits = ["Apple", "banana", "pear", "grapes"];
+const [a, b, c] = fruits;
+console.log(a, b, c);
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [first, second, ...rest] = num;
+console.log(first, second, rest);
+console.log(rest);
+
+let breaks = ["a", "b"];
+console.log(breaks);
+
+let pack1 = ["x", "y"];
+let pack2 = ["z"];
+let bigPack = [...pack1, ...pack2];
+console.log(bigPack);

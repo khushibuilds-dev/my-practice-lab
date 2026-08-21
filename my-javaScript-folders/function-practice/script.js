3e90dad306e5915128ebc973a6569f30a54dba69
Khushi Singh <khu4656@gmail.com>
2026-08-21 23:58:56 +0530

    added sum of two integer

--- my-javaScript-folders/function-practice/script.js
+++ my-javaScript-folders/function-practice/script.js
@@ -3,16 +3,28 @@ num = 4
 function increaseByFive(num) {
 return num += 5;
 }
+let result = increaseByFive(10);
+console.log(result);
 
  console.log("Second last digit Zero cheker");
 
  function checkSecondLastDigit(num) {
     let secondLast =  Math.floor(num/10)%10
-    if(secondLast === 0) {
+    /*if(secondLast === 0) {
         return true;
     } else {
         return false;
-    }
+    }*/
+   return secondLast === 0;
+ }
+let a = checkSecondLastDigit(120);
+
+ console.log(a);
+
+ // task Sum of two integer
+ function sumOfTwo(a, b) {
+    return a+b;
  }
 
- console.log("")
+ // binary to string 
+ 

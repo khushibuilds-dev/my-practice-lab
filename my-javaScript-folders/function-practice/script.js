7ab92ca226bb98cb55cce3a5f729b2b3337b8ba3
Khushi Singh <khu4656@gmail.com>
2026-08-21 23:28:28 +0530

    added second last digit checker function

--- my-javaScript-folders/function-practice/script.js
+++ my-javaScript-folders/function-practice/script.js
@@ -1,5 +1,18 @@
-// increase integer by five
+console.log("increase integer by five");
 num = 4
 function increaseByFive(num) {
 return num += 5;
 }
+
+ console.log("Second last digit Zero cheker");
+
+ function checkSecondLastDigit(num) {
+    let secondLast =  Math.floor(num/10)%10
+    if(secondLast === 0) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+ console.log("")

c94390e69f2af0aa3a94f9bd2f5578d4bf7ff223
Khushi Singh <khu4656@gmail.com>
2026-08-22 10:57:24 +0530

    Practiced triangle

--- my-javaScript-folders/function-practice/script.js
+++ my-javaScript-folders/function-practice/script.js
@@ -66,3 +66,14 @@ row +="#";
    }
    console.log(row);
   }
+
+  let y = 4;
+  for(let i=1; i<=y; i++) {
+   let x = " "
+   for(let j=1; j<=i; j++) {
+     x += j;
+   }
+   console.log(x);
+  }
+
+  

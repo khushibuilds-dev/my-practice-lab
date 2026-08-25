d2a75830ec5ad7feda70a49d3df3f4e038edef04
Khushi Singh <khu4656@gmail.com>
2026-08-26 00:26:26 +0530

    added max number find task

--- my-javaScript-folders/logic-building/script.js
+++ my-javaScript-folders/logic-building/script.js
@@ -2,7 +2,7 @@
 Let I have a string like " khushi " and want to reverse it like this ihsuhk. */
 let str = "Khushi";
 let revWord = ""
-for(i = str.length-1; i>=0; i--) {
+for(let i = str.length-1; i>=0; i--) {
   revWord = revWord + "" + str[i]
 }
 console.log(revWord);
@@ -13,4 +13,26 @@ function reverseString(word) {
 }
 const result = reverseString("Ajay");
 console.log(result);
+
+// Find max number
+function maxNumber(nums) {
+  let max = nums[0];
+  for(let i = 0; i<nums.length; i++ ) {
+    if(nums[i] > max) {
+      max = nums[i];
+    }
+  }
+  return max;
+}
+const maxNums = maxNumber([23, 1, 5, 25, 34, 2]);
+console.log(maxNums);
+
+let arr = [12, 2, 45, 6, 13, 9, 56];
+let max = arr[0];
+for(let i = 0; i<arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+}
+console.log(max);
   

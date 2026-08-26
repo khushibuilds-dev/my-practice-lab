e16f939d75777b7ea2ad82a36a1fcf356e3fc68f
Khushi Singh <khu4656@gmail.com>
2026-08-26 10:32:30 +0530

    Sum of array except self

--- my-javaScript-folders/logic-building/script.js
+++ my-javaScript-folders/logic-building/script.js
@@ -35,4 +35,20 @@ for(let i = 0; i<arr.length; i++) {
     }
 }
 console.log(max);
-  
+/* Sum OF Array Except self Mtlb ke har bar sum krna hai sb elemnt ka lekin jaha se sur karna hai use chhode dene hai
+jaise [1, 3, 5, 8, 9, 23]; isme 0 index per ho to 1 ko chode kar baki ka sum phir 1 index per ho to
+3 ko chhod de aisa hi sb ka sum jo vo elemnt agyega use alg array mai de dna */
+function sumOfArray(arr) {
+  let sum = 0;
+  for(let i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+    let empty = [];
+  for(let i=0; i<arr.length; i++) {
+    empty.push(sum - arr[i]);
+  }
+  return empty;  
+
+}
+const arry= sumOfArray([1, 2, 3, 4]);
+console.log(arry);

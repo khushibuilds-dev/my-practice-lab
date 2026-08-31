9c4c2085e9a2b11721f44a16932ad06915fb9301
Khushi Singh <khu4656@gmail.com>
2026-08-31 14:14:13 +0530

    Sum each row

--- my-javaScript-folders/logic-building/script.js
+++ my-javaScript-folders/logic-building/script.js
@@ -351,7 +351,43 @@ function matrix(arr) {
 matrix([[10, 20], [30, 40]]);
 
 // Sum of 2d element 
-
+function sumElement(arr) {
+  let sum = 0;
+  for(let i=0; i<arr.length; i++) {
+    for(let j=0; j<arr[i].length; j++) {
+      sum += arr[i][j];
+    }
+  }
+  return sum;
+}
+const sum = sumElement([[10, 20], [30, 40]]);
+console.log(sum);
+// find max number in 2d array
+function maxNumber(arr) {
+  let max = arr[0][0];
+  for(let i=0; i<arr.length; i++) {
+    for(let j=0; j<arr[i].length; j++) {
+      if(arr[i][j]>max){
+        max = arr[i][j];
+      }
+    }
+  }
+  return max;
+}
+// sum of each row;
+function SumArr(arr) {
+  let arrEmpty =[];
+  for(let i=0; i<arr.length; i++) {
+    let sum =0;
+    for(let j=0; j<arr[i].length; j++){
+      sum += arr[i][j];
+    }
+    arrEmpty.push(sum);
+  }
+  return arrEmpty;
+}
+const eachSum = SumArr([[2, 1, 3], [3, 3, 2]]);
+console.log(eachSum);
 
 
 

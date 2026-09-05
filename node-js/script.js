fbd82deb21473ee6e2682f7c489df92fd6cd7d63
Khushi Singh <khu4656@gmail.com>
2026-09-05 09:56:32 +0530

    Row wise max

--- node-js/script.js
+++ node-js/script.js
@@ -1,103 +1,128 @@
-console.log("Helo khushi future tech");
-console.log("Khushi singh");
-let fruits = ["Apple", "banana", "pear", "grapes"];
-const [a, b, c] = fruits;
-console.log(a, b, c);
+// console.log("Helo khushi future tech");
+// console.log("Khushi singh");
+// let fruits = ["Apple", "banana", "pear", "grapes"];
+// const [a, b, c] = fruits;
+// console.log(a, b, c);
 
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const [first, second, ...rest] = num;
-console.log(first, second, rest);
-console.log(rest);
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const [first, second, ...rest] = num;
+// console.log(first, second, rest);
+// console.log(rest);
 
-let breaks = ["a", "b"];
-console.log(breaks);
+// let breaks = ["a", "b"];
+// console.log(breaks);
 
-let pack1 = ["x", "y"];
-let pack2 = ["z"];
-let bigPack = [...pack1, ...pack2];
-console.log(bigPack);
+// let pack1 = ["x", "y"];
+// let pack2 = ["z"];
+// let bigPack = [...pack1, ...pack2];
+// console.log(bigPack);
 
-let favFruits = ["Mango", "Grapes", "bananaa", "pear", "Papaya"];
-console.log(favFruits);
-const myFriendsList = ["Kriti", "Anshika", "Ansh", "Rinki", "Ankita", "Palak", "Sanjana", "etc."];
-console.log(myFriendsList[1]);
-console.log(myFriendsList.indexOf());
+// let favFruits = ["Mango", "Grapes", "bananaa", "pear", "Papaya"];
+// console.log(favFruits);
+// const myFriendsList = ["Kriti", "Anshika", "Ansh", "Rinki", "Ankita", "Palak", "Sanjana", "etc."];
+// console.log(myFriendsList[1]);
+// console.log(myFriendsList.indexOf());
 
 
-const myFavPlaces = {
-    north: "Delhi",
-    south: "Kerela"
-};
-console.log(myFavPlaces);
-const user = {
-    name: "Khushi Singh",
-    age: 19,
-    profession: "Student"
+// const myFavPlaces = {
+//     north: "Delhi",
+//     south: "Kerela"
+// };
+// console.log(myFavPlaces);
+// const user = {
+//     name: "Khushi Singh",
+//     age: 19,
+//     profession: "Student"
 
+// }
+// console.log(user);
+// console.log(user.name);
+// const details = {
+//     name: "Khushi",
+//     age: 19,
+//     birth: "4th may 2007"
+// };
+// const{name, age, bir} = details;
+// console.log(bir);
+//  const laptop = {
+//     brand: "HP",
+//     ram: "16GB",
+//     processor: "i7",
+//     Os: "Windows"
+//  };
+//  console.log(laptop.ram);
+//  const{brand, ram, processor} = laptop;
+//  console.log(laptop);
+//  console.log(ram);
+//  console.log(brand);
+//  console.log(laptop['price']);
+//  console.log(ram in laptop, "YE false dega kyuki ye proper nahi dhek raha hai variable dekh rha hai");
+//  console.log("ram" in laptop);
+//  console.log(laptop.hasOwnProperty("ram"));
+
+
+// { 
+// const ginti = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const[a, b, ...rest] = fruits;
+// console.log(rest);
+// console.log(a);
+// const teacher = {
+// hindi: "Ankush Sir",
+// English: "Priyanshu Sir",
+// Math: "Suraj Sir",
+// Science: "Jatin sir",
+// Physics: "Mridiual Sir"
+// }
+
+//  const{hindi, english, ...other} = teacher;
+//  console.log(other);
+
+//  // Knowing something is there in code or not
+//  // is Chemisrty teacher teacher in the list or not
+//  console.log("Chemistry" in teacher); // it will return false
+
+//  // chemistry teacher in list
+
+//  teacher.chemistry= "Jatin Sir";
+//  console.log(teacher);
+
+//  console.log("chemistry" in teacher);
+
+//  // find english teacher
+
+//  console.log(teacher.chemistry);
+// }
+
+
+
+// /* Here i have to build a school manager project wher the school teacher name and subject list
+// with the student name and thier grade. */
+
+// Diagonal sum
+function diagonalSum(mat){
+    let sum = 0;
+    for(let i=0; i<mat.length; i++){
+        sum += mat[i][i];
+    }
+    return sum;
 }
-console.log(user);
-console.log(user.name);
-const details = {
-    name: "Khushi",
-    age: 19,
-    birth: "4th may 2007"
-};
-const{name, age, bir} = details;
-console.log(bir);
- const laptop = {
-    brand: "HP",
-    ram: "16GB",
-    processor: "i7",
-    Os: "Windows"
- };
- console.log(laptop.ram);
- const{brand, ram, processor} = laptop;
- console.log(laptop);
- console.log(ram);
- console.log(brand);
- console.log(laptop['price']);
- console.log(ram in laptop, "YE false dega kyuki ye proper nahi dhek raha hai variable dekh rha hai");
- console.log("ram" in laptop);
- console.log(laptop.hasOwnProperty("ram"));
+console.log(diagonalSum([[1, 2, 3], [4, 5, 6], [2, 3, 4]]));
+// Row wise Maximum
+function rowWiseMax(mat){
+    let row = [];
 
-
-{ 
-const ginti = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const[a, b, ...rest] = fruits;
-console.log(rest);
-console.log(a);
-const teacher = {
-hindi: "Ankush Sir",
-English: "Priyanshu Sir",
-Math: "Suraj Sir",
-Science: "Jatin sir",
-Physics: "Mridiual Sir"
+    for(let i=0; i<mat.length; i++){
+        let max = mat[i][0];
+        for(let j=0; j<mat[i].length; j++){
+            if(mat[i][j]>max){
+                max = mat[i][j];
+            } 
+        }
+        row.push(max)
+    }
+    return row;
 }
-
- const{hindi, english, ...other} = teacher;
- console.log(other);
-
- // Knowing something is there in code or not
- // is Chemisrty teacher teacher in the list or not
- console.log("Chemistry" in teacher); // it will return false
-
- // chemistry teacher in list
-
- teacher.chemistry= "Jatin Sir";
- console.log(teacher);
-
- console.log("chemistry" in teacher);
-
- // find english teacher
-
- console.log(teacher.chemistry);
-}
-
-
-
-/* Here i have to build a school manager project wher the school teacher name and subject list
-with the student name and thier grade. */
 
 
 

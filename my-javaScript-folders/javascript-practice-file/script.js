e5fa8ac4ab28659de9baa64787681ece3c5bde6c
Khushi Singh <khu4656@gmail.com>
2026-09-05 14:40:25 +0530

    Added tipCalculator function

--- my-javaScript-folders/javascript-practice-file/script.js
+++ my-javaScript-folders/javascript-practice-file/script.js
@@ -1,244 +1,252 @@
-// /*console.log("Khushi", "Welcome to my photo lab!", );
-// console.log("My Birthday:", 2007);
-// console.log("user", "Khushi");
-// console.log("My Age:", 16);
-// console.log("khushi", 200);
-// let a = "Khushi Singh";
-// console.log("My name is;" + " " + a);
-// let myAge;
-// console.log("My Age is:", myAge);
-// let b = true;
-// console.log("I am a student:", b);
-// let firstName = "Khushi";
-// let lastName = "Singh";
-// console.log("My full name is;" + " " + firstName + " " + lastName);
-// let x = 5;
-// x ++;
-// console.log(x);
-// let y = 10;
-// y --;
-// console.log(y);
-// let z = 17;
-// let w = 2;
-// let quo = Math.floor(z/w);
-// console.log(quo);
-// let rem = z%w;
-// console.log(rem);
-// console.log(z*w);
-// console.log(z+w);
-// let radius = 4;
-// let pi = 3;
-// let r = radius;
-// console.log(pi * r * r);
-// console.log(2 * pi * r);
-// let user = true;
-// if(user){
-//     console.log("Please sign in to continue");
-// }
-// else{
-//     console.log("Welcome to my photo lab!");
-// }
+// // /*console.log("Khushi", "Welcome to my photo lab!", );
+// // console.log("My Birthday:", 2007);
+// // console.log("user", "Khushi");
+// // console.log("My Age:", 16);
+// // console.log("khushi", 200);
+// // let a = "Khushi Singh";
+// // console.log("My name is;" + " " + a);
+// // let myAge;
+// // console.log("My Age is:", myAge);
+// // let b = true;
+// // console.log("I am a student:", b);
+// // let firstName = "Khushi";
+// // let lastName = "Singh";
+// // console.log("My full name is;" + " " + firstName + " " + lastName);
+// // let x = 5;
+// // x ++;
+// // console.log(x);
+// // let y = 10;
+// // y --;
+// // console.log(y);
+// // let z = 17;
+// // let w = 2;
+// // let quo = Math.floor(z/w);
+// // console.log(quo);
+// // let rem = z%w;
+// // console.log(rem);
+// // console.log(z*w);
+// // console.log(z+w);
+// // let radius = 4;
+// // let pi = 3;
+// // let r = radius;
+// // console.log(pi * r * r);
+// // console.log(2 * pi * r);
+// // let user = true;
+// // if(user){
+// //     console.log("Please sign in to continue");
+// // }
+// // else{
+// //     console.log("Welcome to my photo lab!");
+// // }
 
-// let userAge = 20;
-// if(userAge >=18){
-//     console.log("You are eligible to vote");
-// }else{
-//     console.log("You are not eligible to vote");
-// }
-// let inLoggedIn ="true";
-// if(inLoggedIn === "true"){
-//     console.log("Welcome to my photo lab!");
-// }else if(inLoggedIn === "false"){
-//     console.log("Please sign in to continue");
-// }else{
-//     console.log("Please sign in to continue");
-// }
+// // let userAge = 20;
+// // if(userAge >=18){
+// //     console.log("You are eligible to vote");
+// // }else{
+// //     console.log("You are not eligible to vote");
+// // }
+// // let inLoggedIn ="true";
+// // if(inLoggedIn === "true"){
+// //     console.log("Welcome to my photo lab!");
+// // }else if(inLoggedIn === "false"){
+// //     console.log("Please sign in to continue");
+// // }else{
+// //     console.log("Please sign in to continue");
+// // }
 
-// {
-//     let letter = "A";
-//     console.log(letter.charCodeAt(0));
-// let letter1 = "B";
-// console.log(letter1.charCodeAt(0));
-// }
+// // {
+// //     let letter = "A";
+// //     console.log(letter.charCodeAt(0));
+// // let letter1 = "B";
+// // console.log(letter1.charCodeAt(0));
+// // }
 
-// {
-//     let char = String.fromCharCode(66);
-//     console.log(char);
-//    let char1 = String.fromCharCode(67);
-//    console.log(char1);
-// }
+// // {
+// //     let char = String.fromCharCode(66);
+// //     console.log(char);
+// //    let char1 = String.fromCharCode(67);
+// //    console.log(char1);
+// // }
 
-// {
-//     let phrase = "God is great";
-//     console.log(phrase.includes("great"));
+// // {
+// //     let phrase = "God is great";
+// //     console.log(phrase.includes("great"));
 
-//     let phrase1 = "Khushi Singh - Full Stack Web Developer";
-//     console.log(phrase1.includes("Mern Stack Web Developer"));
-// output: false
-// }
+// //     let phrase1 = "Khushi Singh - Full Stack Web Developer";
+// //     console.log(phrase1.includes("Mern Stack Web Developer"));
+// // output: false
+// // }
 
-// {
-//     let phrase2 = "Khushi Singh - Full Stack Web Developer";
-//     console.log(phrase2.slice(0, 6));
-//      output: Khushi
+// // {
+// //     let phrase2 = "Khushi Singh - Full Stack Web Developer";
+// //     console.log(phrase2.slice(0, 6));
+// //      output: Khushi
 
-//     let phrase3 = "Khushi Singh - Full Stack Web Developer";
-//     console.log(phrase3.slice(7, 12));
-//      output: Singh
-// }
+// //     let phrase3 = "Khushi Singh - Full Stack Web Developer";
+// //     console.log(phrase3.slice(7, 12));
+// //      output: Singh
+// // }
 
-// let phrase4 = "Khushi Singh - Full Stack Web Developer";
-// let name = phrase4.indexOf("Singh");
-// console.log(name);
-//  output: 7
-// console.log(phrase4.slice(0, name));
-//  output: Khushi
+// // let phrase4 = "Khushi Singh - Full Stack Web Developer";
+// // let name = phrase4.indexOf("Singh");
+// // console.log(name);
+// //  output: 7
+// // console.log(phrase4.slice(0, name));
+// //  output: Khushi
 
-// let phrase5 = "Everyone is welcome to my photo lab! but you have to sign in first to continue than you can enjoy the photo lab!";
-// console.log(phrase5.indexOf("my"));
-//  output: 23
+// // let phrase5 = "Everyone is welcome to my photo lab! but you have to sign in first to continue than you can enjoy the photo lab!";
+// // console.log(phrase5.indexOf("my"));
+// //  output: 23
 
-// console.log(phrase5.indexOf("!"));
-//  output: 35
+// // console.log(phrase5.indexOf("!"));
+// //  output: 35
 
-// console.log(phrase5.slice(23, 35));
-//  output: my photo lab
+// // console.log(phrase5.slice(23, 35));
+// //  output: my photo lab
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2 Multiply the two together and store the result in myDogAge
-// 3 Log myDogAge to the console
-// {
-//     let myAge = 35;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
-// output 
-// }
+// // 1. Create two variables, myAge and humanDogRatio
+// // 2 Multiply the two together and store the result in myDogAge
+// // 3 Log myDogAge to the console
+// // {
+// //     let myAge = 35;
+// // let humanDogRatio = 7;
+// // let myDogAge = myAge * humanDogRatio;
+// // console.log(myDogAge);
+// // output 
+// // }
 
-// {
-//     create a variable, bonusPoint. Initialize it as 50. Increase to 100.
-//     Decrease it down to 25, and then fianally increase it ti 70
-//     let bonusPoints = 50;
-//     console.log(bonusPoints);
-//      output will be 50
-//     bonusPoints = bonusPoints + 50;
-//     console.log(bonusPoints);
-//     output will be 100
+// // {
+// //     create a variable, bonusPoint. Initialize it as 50. Increase to 100.
+// //     Decrease it down to 25, and then fianally increase it ti 70
+// //     let bonusPoints = 50;
+// //     console.log(bonusPoints);
+// //      output will be 50
+// //     bonusPoints = bonusPoints + 50;
+// //     console.log(bonusPoints);
+// //     output will be 100
 
-//     bonusPoints = bonusPoints - 75
-//     console.log(bonusPoints);
-//      output will be 25
-
-
-// }
-// {
-//     intialize the count as 0
-//      listen for clicks on the increment button
-//      increment the count variable when the button is clicked
-//      change the count-el in the HTML to reflect the new count count
-
-//     function increment() {
-//         console.log("The button was clicked");
-
-//     }
-// }
-
-// {
-//    const rating1 = 12;
-//    const rating2 = 12.89;
-//    const rating3 = 12.12;
-//    function calculateAverageRating(){
-//     const sum = rating1+rating2+rating3;
-//     return Math.floor(sum/3);
-
-//    }
-//    console.log(calculateAverageRating());
-
-// }
-
-// const truthyOrFalshy = "true";
-// console.log (Boolean("truthyOrFalshy"));
-// const freeCodeCamp = "";
-// console.log(Boolean(freeCodeCamp));
-
-// const hasDeveloperJob = "true";
-// if (hasDeveloperJob) {
-//     console.log("khushi has a job to learn new things everday.");
-// }
-
-// const isKhushiAGamer = false;
-// if (isKhushiAGamer) {
-// console.log("Khushi is a web developer");
+// //     bonusPoints = bonusPoints - 75
+// //     console.log(bonusPoints);
+// //      output will be 25
 
 
-// Task: The Movie Theatre System
-// Imagine you are making a system for a movie theatre.
-// To watch a Horror Movie,
-//  a person must be 18 years old AND have a Ticket.
-//  Your Variables:
-//  javascriptlet hasTicket = 1; // 1 means Yes, 0 means No
-// let isEighteen = 0; // 1 means Yes, 0 means No
-// What you need to do:Write a simple JavaScript if-else code using a Bitwise Operator to check if the person can watch the movie or not.
-// If they can watch,
-//  print "Welcome".
-//  If they cannot watch, print "Go Home". 
-//  let isEighteen = 0;
-//  let hasTicket = 1;
-//  if (isEighteen & hasTicket) {
-//     console.log("Welcome");
+// // }
+// // {
+// //     intialize the count as 0
+// //      listen for clicks on the increment button
+// //      increment the count variable when the button is clicked
+// //      change the count-el in the HTML to reflect the new count count
 
-//   }else{
-//     console.log("Go home");
-//   }
+// //     function increment() {
+// //         console.log("The button was clicked");
 
-//   let umer = 12;
-//   if(umer<18) {
-// console.log("You can watch movie");
-//   }
+// //     }
+// // }
+
+// // {
+// //    const rating1 = 12;
+// //    const rating2 = 12.89;
+// //    const rating3 = 12.12;
+// //    function calculateAverageRating(){
+// //     const sum = rating1+rating2+rating3;
+// //     return Math.floor(sum/3);
+
+// //    }
+// //    console.log(calculateAverageRating());
+
+// // }
+
+// // const truthyOrFalshy = "true";
+// // console.log (Boolean("truthyOrFalshy"));
+// // const freeCodeCamp = "";
+// // console.log(Boolean(freeCodeCamp));
+
+// // const hasDeveloperJob = "true";
+// // if (hasDeveloperJob) {
+// //     console.log("khushi has a job to learn new things everday.");
+// // }
+
+// // const isKhushiAGamer = false;
+// // if (isKhushiAGamer) {
+// // console.log("Khushi is a web developer");
+
+
+// // Task: The Movie Theatre System
+// // Imagine you are making a system for a movie theatre.
+// // To watch a Horror Movie,
+// //  a person must be 18 years old AND have a Ticket.
+// //  Your Variables:
+// //  javascriptlet hasTicket = 1; // 1 means Yes, 0 means No
+// // let isEighteen = 0; // 1 means Yes, 0 means No
+// // What you need to do:Write a simple JavaScript if-else code using a Bitwise Operator to check if the person can watch the movie or not.
+// // If they can watch,
+// //  print "Welcome".
+// //  If they cannot watch, print "Go Home". 
+// //  let isEighteen = 0;
+// //  let hasTicket = 1;
+// //  if (isEighteen & hasTicket) {
+// //     console.log("Welcome");
+
+// //   }else{
+// //     console.log("Go home");
+// //   }
+
+// //   let umer = 12;
+// //   if(umer<18) {
+// // console.log("You can watch movie");
+// //   }
   
-//   const botName = "MathBot";
-// const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
-// console.log(greeting);
-// console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+// //   const botName = "MathBot";
+// // const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+// // console.log(greeting);
+// // console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
 
-// console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
-// const randomNum = Math.random();
-// console.log(randomNum);
+// // console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+// // const randomNum = Math.random();
+// // console.log(randomNum);
 
-// console.log(Math.max(3,23,1,3));
-// console.log(Math.max(23,1,2,34,4));
+// // console.log(Math.max(3,23,1,3));
+// // console.log(Math.max(23,1,2,34,4));
 
-// console.log(Math.ceil(2.34));
-// console.log(Math.ceil(3.6));
-// console.log(Math.ceil(45.4));
+// // console.log(Math.ceil(2.34));
+// // console.log(Math.ceil(3.6));
+// // console.log(Math.ceil(45.4));
  
-/* 
+// /* 
 
-Aapko ek aisa array diya jayega jo pehle chote se bade order (sorted) mein tha, lekin use kuch baar right rotate kar diya gaya hai.
- Aapko us array ka sabse bada (maximum) element dhoondna hai aur use return karna hai.
-Constraint 1: Aapko .sort() function ka use nahi karna hai (kyunki interviewers .sort() use karne se mana karte hain).
-Constraint 2: Original array ke andar hi logic lagana hai.
+// Aapko ek aisa array diya jayega jo pehle chote se bade order (sorted) mein tha, lekin use kuch baar right rotate kar diya gaya hai.
+//  Aapko us array ka sabse bada (maximum) element dhoondna hai aur use return karna hai.
+// Constraint 1: Aapko .sort() function ka use nahi karna hai (kyunki interviewers .sort() use karne se mana karte hain).
+// Constraint 2: Original array ke andar hi logic lagana hai.
 
-function sortArrMax(arr){
-    let max = arr[0];
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] > max){
-            max = arr[i];
-        }
-    }
-    return max;
-}
-function sortArrMaxOneLine(arr){
-  return Math.max(... arr);
-}
-console.log(sortArrMaxOneLine([2, 3, 4, 5, 7]));
+// function sortArrMax(arr){
+//     let max = arr[0];
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+// function sortArrMaxOneLine(arr){
+//   return Math.max(... arr);
+// }
+// console.log(sortArrMaxOneLine([2, 3, 4, 5, 7]));
 
-Character Counter (String Object Round)Aapko ek string di jayega (jaise "hello"). 
-Aapko ek function banana hai jo yeh count kare ki us string mein har ek character (alphabets) kitni baar aaya hai, aur use ek JavaScript Object format mein return kare.
-Constraint: Output ek object {} hona chahiye jahan character uski key ho aur count uska value.📋
- Example:Input String: "hello"
-Output Object: { h: 1, e: 1, l: 2, o: 1 } (Kyunki 'l' do baar aaya hai, baaki sab ek baar). 
+// Character Counter (String Object Round)Aapko ek string di jayega (jaise "hello"). 
+// Aapko ek function banana hai jo yeh count kare ki us string mein har ek character (alphabets) kitni baar aaya hai, aur use ek JavaScript Object format mein return kare.
+// Constraint: Output ek object {} hona chahiye jahan character uski key ho aur count uska value.📋
+//  Example:Input String: "hello"
+// Output Object: { h: 1, e: 1, l: 2, o: 1 } (Kyunki 'l' do baar aaya hai, baaki sab ek baar). 
 
-*/
+// */
 
+// Tip Calculator
+// function tipCalculator(billAmount, tipPer){
+//     let tip = billAmount * (tipPer/100);
+//     let totalBill = billAmount + tip;
+//     return `आपका असली बिल ${billAmount} रुपये है। ${tipPer}% के हिसाब से टिप ${tip} रुपये बनती है। आपको कुल मिलाकर ${totalBill} रुपये देने होंगे।`;
+// }
+
+// console.log(tipCalculator(2000, 5));
 

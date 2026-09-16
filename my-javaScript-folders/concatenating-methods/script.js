f71ffa2d10ec1a8ca52ab587e4cd62588cdaff4a
Khushi Singh <khu4656@gmail.com>
2026-09-16 16:10:25 +0530

    feat: focus script.js on concatenating practice
    Restored and updated console log statements and variable declarations.

---
+++ my-javaScript-folders/concatenating-methods/script.js
@@ -0,0 +1,17 @@
+console.log("Khushi", "Welcome to my photo lab!", )
+console.log("My Birthday:", 2007);
+console.log("user", "Khushi");
+console.log("My Age:", 16);
+console.log("khushi", 200);
+let a = "Khushi Singh";
+console.log("My name is;" + " " + a);
+let myAge;
+console.log("My Age is:", myAge);
+let b = true;
+console.log("I am a student:", b);
+let firstName = "Khushi";
+let lastName = "Singh";
+console.log("My full name is;" + " " + firstName + " " + lastName);
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+console.log(greeting);
